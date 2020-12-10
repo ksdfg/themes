c28750c5010f89168f19b0a6e4c62100e7fb5922
ksdfg <ksdfg123@gmail.com>
2020-12-10 10:13:24 +0530

    add another tray icon for teams activity notif
    Signed-off-by: ksdfg <ksdfg123@gmail.com>

--- custom-icons/teams/update.sh
+++ custom-icons/teams/update.sh
@@ -6,11 +6,12 @@ sudo echo
 declare -a arr=(
     "Error-Systray16x16@2x"
     "Presence-NoActivity-Systray16x16@2x"
+    "Presence-Activity-Systray16x16@2x"
 )
 
 for icon in "${arr[@]}"; do
     echo "Backing up existing $icon"
-    sudo cp /usr/share/teams/resources/assets/$icon.png $HOME/.themes/custom-icons/teams/old/$icon.png
+    cp /usr/share/teams/resources/assets/$icon.png $HOME/.themes/custom-icons/teams/old/$icon.png
 
     echo "Creating new $icon"
     sudo cp $HOME/.themes/custom-icons/teams/new/$icon.png /usr/share/teams/resources/assets/$icon.png
